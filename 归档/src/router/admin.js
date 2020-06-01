ba06df7a8a5290327a3c4866d4dd03326af60265
MengQingPeng111 <211385081@qq.com>
2020-06-01 20:07:18 +0800

    这是一个项目

---
+++ 归档/src/router/admin.js
@@ -0,0 +1,11 @@
+import  Login  from '@/pages/admin/Login.vue';
+
+const admins = [
+    {
+        path: "/admin/login",
+        name: "admin.login",
+        component: Login,
+    }
+];
+
+export default admins;
